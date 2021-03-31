5fc604b8ec8198421684ebc3013fd0a30ab77564
jonyelson <jonyelson20@gmail.com>
2021-03-31 00:08:27 -0400

    Feat: created the user registry

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
-/*import {signUp} from "./endpoints/signUp";
-import {login} from "./endpoints/login";
+import {signUp} from "./endpoints/signUp";
+/*import {login} from "./endpoints/login";
 import {getUserProfile} from "./endpoints/getUserProfile";
 import {getUser} from "./endpoints/getUser";
 import {createRecipe} from "./endpoints/createRecipe";
@@ -16,8 +16,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-/*app.post('/signup', signUp);
-app.post('/login', login);
+app.post('/signup', signUp);
+/*app.post('/login', login);
 app.get('/user/profile', getUserProfile);
 app.get('/user/:id', getUser);
 app.post('/recipe', createRecipe);
